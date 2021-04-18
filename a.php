5c4365e8fbdcdaf963e8a0663372f434074f318a
julic123 <60428120+julic123@users.noreply.github.com>
2021-04-19 02:50:31 +0900

    Update a.php

--- a.php
+++ a.php
@@ -1,4 +1,4 @@
-$location = '이미지 주소';
+$location = 'https://github.com/julic123/img/blob/71d673ebbd7a4115733987ee731468b594b24f69/tutorial_answer.jpg';
 $extTemp = explode('.',basename($location));
 $ext = $extTemp[1]; if($ext == 'jpg') { $ext = 'jpeg'; }
 header('Content-Type: image/'.$ext.'');
