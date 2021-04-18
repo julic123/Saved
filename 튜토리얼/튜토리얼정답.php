ef93d8215d20dc2865c6f112da3a1dbb2eb24d99
julic123 <60428120+julic123@users.noreply.github.com>
2021-04-19 02:14:09 +0900

    Update 튜토리얼정답.php

--- 튜토리얼/튜토리얼정답.php
+++ 튜토리얼/튜토리얼정답.php
@@ -1,4 +1,4 @@
-$location = '정답.png';
+$location = '\정답.png';
 $extTemp = explode('.',basename($location));
 $ext = $extTemp[1]; if($ext == 'jpg') { $ext = 'jpeg'; }
 header('Content-Type: image/'.$ext.'');
