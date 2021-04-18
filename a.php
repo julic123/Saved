1cfaf27c6adab71e525cf07493f9f9c8d2bf9bf6
julic123 <60428120+julic123@users.noreply.github.com>
2021-04-19 03:20:02 +0900

    Update a.php

--- a.php
+++ a.php
@@ -1,4 +1,4 @@
-$location = 'https://github.com/julic123/img/blob/main/tutorial_answer.jpg?raw=true';
+$location = '54-544029_ubuntu-wallpaper-4k.jpg';
 $extTemp = explode('.',basename($location));
 $ext = $extTemp[1]; if($ext == 'jpg') { $ext = 'jpeg'; }
 header('Content-Type: image/'.$ext.'');
