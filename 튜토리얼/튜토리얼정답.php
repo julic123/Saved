8f25c0e79d341700d2f17897614ee993961796d9
julic123 <60428120+julic123@users.noreply.github.com>
2021-04-19 02:32:23 +0900

    Delete 튜토리얼 directory

--- 튜토리얼/튜토리얼정답.php
+++
@@ -1,5 +0,0 @@
-$location = '\정답.png';
-$extTemp = explode('.',basename($location));
-$ext = $extTemp[1]; if($ext == 'jpg') { $ext = 'jpeg'; }
-header('Content-Type: image/'.$ext.'');
-readfile($location);
