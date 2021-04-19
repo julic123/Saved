aee0bbd993e40ca083b59ba34ded69ac4e65930f
julic123 <60428120+julic123@users.noreply.github.com>
2021-04-19 13:39:29 +0900

    Delete a.php

--- a.php
+++
@@ -1,5 +0,0 @@
-$location = '54-544029_ubuntu-wallpaper-4k.jpg';
-$extTemp = explode('.',basename($location));
-$ext = $extTemp[1]; if($ext == 'jpg') { $ext = 'jpeg'; }
-header('Content-Type: image/'.$ext.'');
-readfile($location);
